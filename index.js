0128ba077d554f0c979a698860cbe12f9f5ca068
KeithWang <570109400@qq.com>
2018-04-17 15:08:29 +0800

    君心

---
+++ index.js
@@ -0,0 +1,6 @@
+import { Button } from './public/src/component/Button.jsx';
+import { LocaleProvider, LocaleValue, LocaleLabel } from './public/src/component/LocaleProvider.jsx';
+import en_US from './public/src/component/locales/en_US';
+import zh_CN from './public/src/component/locales/zh_CN';
+
+export { Button, LocaleProvider, LocaleValue, LocaleLabel, en_US, zh_CN };
